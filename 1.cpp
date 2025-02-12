cdee265bb055e343918993536f2192a229e320cb
parthsapariya745 <parthsapariyait7@gmail.com>
2025-02-12 13:48:14 +0530

    question-c++

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+// QUESTION-BANK C/C++
+
+// 1.
+// -> programming is write a code to understand for computer and output run to terminal.
+
+// -> Translator is a user write a code for convert into binary, computer provide output is binary and binary output convert into user understand output. 
+
+// -> There are two type of translators:
+//    1) Compiler 2) Interpreter
+//    c / c++ translator using compiler
+
+// -> Difference between Compiler and Interpreter
+// >> Compiler : compiler is convert full code of the binary
+// >> Interpreter : Interpreter is convert line by line code of the binary
